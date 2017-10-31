f879e4fa9077ac692ea021003a1c0bca60b3a683
Ataraxxia <fabulousmcc@gmail.com>
2017-10-31 21:02:09 +0100

    improv

--- AllegroPhotoCrawler.py
+++ AllegroPhotoCrawler.py
@@ -10,6 +10,7 @@ HEADER = {"User-Agent": "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML
 
 def downloadWebpage(url, values=''):
     data = parse.urlencode(values)
+    print('URL: {0}'.format(url + '?' + data))
     req = request.Request(url + '?' + data, headers=HEADER)
     response = request.urlopen(req)
     the_page = response.read()
@@ -63,29 +64,34 @@ if __name__ == "__main__":
     parser.add_argument('--phrase', type=str, default='', help='Search keyword')
     parser.add_argument('--output-path', type=str, default='', help='Path to directory where files will be saved')
     parser.add_argument('--connections', type=int, default=10, help='How many requests to send in pararell')
+    parser.add_argument('--pages', type=str, default='0', help='Range of pages you want to search for images specified as FirstPage,LastPage. Default settings searches all pages it can')
     args=parser.parse_args()
 
     category = args.category
     phrase = args.phrase
     output_path = args.output_path
     connections = args.connections
+    pages_range = args.pages
 
     allegro_url = 'https://allegro.pl'
     allegro_url = allegro_url + '/kategoria/' + category
     directory_name = output_path + '\\' + category + '_' + phrase
     createDirectory(directory_name)
 
-    values = { }
-    webpage = downloadWebpage(allegro_url, values).decode('unicode-escape') #Full-sized images have names encoded with unicode
-    page_count = int(re.findall('data-maxpage="(.*?)"', webpage)[0])
+    if pages_range == '0':
+        values = { }
+        webpage = downloadWebpage(allegro_url, values).decode('unicode-escape') #Full-sized images have names encoded with unicode
+        firstPage = 1
+        lastPage = int(re.findall('data-maxpage="(.*?)"', webpage)[0])
+    else:
+        pages_range = pages_range.split(',')
+        firstPage = int(pages_range[0])
+        lastPage = int(pages_range[1])
 
-    for p in range(1, page_count + 1):
+    for p in range(firstPage, lastPage + 1):
         start_time = time.time()
+        print('Starting page {0}'.format(p))
         values = { 'string' : phrase, 'p' : p}
-#        if phrase != '':
-#            values = { 'string' : phrase, 'p' : p}
-#        else:
-#            values = { 'p' : p }
 
         webpage = downloadWebpage(allegro_url, values).decode('unicode-escape')
         matches = re.findall('("https(.*?)allegroimg.com/original(.*?)")', webpage)
